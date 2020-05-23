10ca8898f01f4b918522bf7052e59cdf4fc35344
= <=>
2020-05-23 20:34:10 +0530

    created array list of products to be displayed on view

--- app/src/main/java/com/raunakgarments/MainActivity.kt
+++ app/src/main/java/com/raunakgarments/MainActivity.kt
@@ -3,8 +3,11 @@ package com.raunakgarments
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.raunakgarments.model.Product
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val products = arrayListOf<Product>()
+        for(i in 0..100) {
+            products.add(Product(title = "red colored cotton t-shirt", photoUrl = "http://via.placeholder.com/200x200", price = 1.99))
         }
+
+        recyler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ProductAdapter(products)
+        }
+
+    }
 }
