6265003794cb30a03df7fd1aaabc352e668cda93
= <=>
2020-05-30 00:54:53 +0530

    Numbers are being displayed now.

--- app/src/main/java/com/raunakgarments/ProductDetails.kt
+++ app/src/main/java/com/raunakgarments/ProductDetails.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.product_details.*
+import kotlin.Double.Companion.POSITIVE_INFINITY
 
 class ProductDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,7 @@ class ProductDetails : AppCompatActivity() {
         setContentView(R.layout.product_details)
 
         val title = intent.getStringExtra("title")
-        val price = intent.getStringArrayExtra("price")
-        val text = title +" with price = "
-        product_name.setText(text)
+        val price = intent.getDoubleExtra("price", POSITIVE_INFINITY)
+        product_name.text = title +" with price = " + price
     }
 }
