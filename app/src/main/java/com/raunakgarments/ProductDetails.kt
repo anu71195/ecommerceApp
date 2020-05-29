0eeb9fad39086ec85b1dbee732ab3c758cacc2a4
= <=>
2020-05-30 02:41:58 +0530

    Created availability function for see availability button.

--- app/src/main/java/com/raunakgarments/ProductDetails.kt
+++ app/src/main/java/com/raunakgarments/ProductDetails.kt
@@ -2,6 +2,7 @@ package com.raunakgarments
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.product_details.*
@@ -16,5 +17,9 @@ class ProductDetails : AppCompatActivity() {
         val price = intent.getDoubleExtra("price", POSITIVE_INFINITY)
         Picasso.get().load(intent.getStringExtra("imageURL")).into(photo)
         product_name.text = title +"\n price = \u20B9" + price
+
+        availability.setOnClickListener {
+            AlertDialog.Builder(this).setMessage("It's in stock!").create().show()
+        }
     }
 }
