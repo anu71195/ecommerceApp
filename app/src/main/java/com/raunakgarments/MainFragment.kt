ff8b1705ace36e0c6c648749629ec5d514801f64
anurag <anu71195@gmail.com>
2020-06-12 19:12:49 +0530

    using room to show the data without internet after it is loaded once.

--- app/src/main/java/com/raunakgarments/MainFragment.kt
+++ app/src/main/java/com/raunakgarments/MainFragment.kt
@@ -9,7 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.google.gson.Gson
+import com.raunakgarments.database.AppDatabase
+import com.raunakgarments.database.DatabaseProduct
 import com.raunakgarments.model.Product
 import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
@@ -27,11 +30,22 @@ class MainFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
 
         doAsync {
-            val json = URL("https://finepointmobile.com/data/products.json").readText()
 
+            val db = Room.databaseBuilder(
+                requireActivity().applicationContext,
+                AppDatabase::class.java, "productDatabase"
+            ).build()
+            val productsFromDatabase = db.productDao().getAll()
+
+            val products = productsFromDatabase.map {
+                Product(
+                    it.title,
+                    "https://5.imimg.com/data5/RL/WH/OR/SELLER-51723387/blank-tshirt-500x500.jpg",
+                    it.price,
+                    true
+                )
+            }
             uiThread {
-                d("anurag","json: $json")
-                val products = Gson().fromJson(json, Array<Product>::class.java).toList()
 
                 root.recyler_view.apply {
                     layoutManager = GridLayoutManager(activity, 2)
@@ -40,6 +54,8 @@ class MainFragment : Fragment() {
                 }
             }
         }
+
+//        root.progressBar.visibility = View.GONE
 
         val categories = listOf("jeans", "Socks", "Suits", "Skirts", "Dresses", "denims", "pants", "Jackets", "shorts", "payjamas")
 
