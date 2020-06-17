7cdc041f5fae068bffb9438ce562fa72adfca7d4
anurag <anu71195@gmail.com>
2020-06-17 17:22:51 +0530

    Using GitHub for a RESTful JSON API to create and load json data.

--- app/src/main/java/com/raunakgarments/repos/ProductsRepository.kt
+++ app/src/main/java/com/raunakgarments/repos/ProductsRepository.kt
@@ -8,7 +8,7 @@ import java.net.URL
 class ProductsRepository {
     fun getAllProducts(): Single<List<Product>> {
         return Single.create<List<Product>> {
-            val json = URL("https://finepointmobile.com/data/products.json").readText()
+            val json = URL("https://gist.githubusercontent.com/anu71195/3892796549c5e22a2ba591eb3fb723b7/raw/152e420b40578325f4c4babb3f2e84905eb1c0e5/shopping_products.json").readText()
             val products = Gson().fromJson(json, Array<Product>::class.java).toList()
             it.onSuccess(products)
         }
