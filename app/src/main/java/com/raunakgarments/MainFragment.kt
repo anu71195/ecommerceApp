e9021f6ef36be2c80993e1d58b9d87b40b163129
= <=>
2020-06-28 01:54:01 +0530

    Shifted to retrofit and kotlin coroutines.

--- app/src/main/java/com/raunakgarments/MainFragment.kt
+++ app/src/main/java/com/raunakgarments/MainFragment.kt
@@ -9,6 +9,9 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +24,9 @@ import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.categoriesRecylerView
 
 class MainFragment : Fragment() {
+
+    lateinit var viewModel: MainFragmentViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -50,35 +56,40 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val productsRepository = ProductsRepository().getAllProducts()
-        loadRecyclerView(productsRepository)
 
-        searchButton.setOnClickListener {
-            loadRecyclerView(ProductsRepository().searchForProducts(searchTerm.text.toString()))
-        }
+        viewModel = ViewModelProviders.of(requireActivity()).get(MainFragmentViewModel::class.java)
+
+        viewModel.products.observe(requireActivity(), Observer {
+            loadRecyclerView(it)
+        })
+
+        viewModel.setup()
+//        val productsRepository = ProductsRepository().getAllProducts()
+//        loadRecyclerView(productsRepository)
+//
+//        searchButton.setOnClickListener {
+//            loadRecyclerView(ProductsRepository().searchForProducts(searchTerm.text.toString()))
+//        }
     }
 
-    fun loadRecyclerView(productsRepository: Single<List<Product>>) {
-        val single = productsRepository
-        .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                d("Anurag", "success")
-                recyler_view.apply {
-                    layoutManager = GridLayoutManager(activity, 2)
-                    adapter = ProductAdapter(it) { extraTitle, extraImageUrl, extraPrice, photoView ->
-                        val intent = Intent(activity, ProductDetails::class.java)
-                        intent.putExtra("title", extraTitle)
-                        intent.putExtra("price", extraPrice)
+    fun loadRecyclerView(productsRepository: List<Product>) {
+
+        recyler_view.apply {
+            layoutManager = GridLayoutManager(activity, 2)
+            adapter = ProductAdapter(productsRepository) { extraTitle, extraImageUrl, extraPrice, photoView ->
+                val intent = Intent(activity, ProductDetails::class.java)
+                intent.putExtra("title", extraTitle)
+                intent.putExtra("price", extraPrice)
 //                        intent.putExtra("imageURL", extraImageUrl)
-                        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity as AppCompatActivity, photoView, "photoToAnimate")
-                        startActivity(intent, options.toBundle())
-                    }
-                }
-                progressBar.visibility = View.GONE
-            }, {
-                d("anurag", " error :( ${it.message}")
-            })
+                val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    activity as AppCompatActivity,
+                    photoView,
+                    "photoToAnimate"
+                )
+                startActivity(intent, options.toBundle())
+            }
+        }
+        progressBar.visibility = View.GONE
     }
 }
 
