99136e6b354888d236dfeaac1db3453f81aceca7
= <anu71195@gmail.com>
2020-07-20 23:48:52 +0530

    Code Refactoring.

--- app/src/main/java/com/raunakgarments/MainFragmentViewModel.kt
+++ app/src/main/java/com/raunakgarments/MainFragmentViewModel.kt
@@ -1,26 +1,26 @@
-package com.raunakgarments
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.raunakgarments.model.Product
-import com.raunakgarments.repos.ProductsRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
-class MainFragmentViewModel : ViewModel() {
-    val products = MutableLiveData<List<Product>>()
-
-    fun setup() {
-        viewModelScope.launch(Dispatchers.Default) {
-            products.postValue(ProductsRepository().fetchAllProductsRetrofit())
-        }
-    }
-
-    fun search(term: String) {
-        viewModelScope.launch(Dispatchers.Default) {
-            products.postValue(ProductsRepository().searchForProducts(term))
-        }
-    }
-
-}
+//package com.raunakgarments
+//
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.viewModelScope
+//import com.raunakgarments.model.Product
+//import com.raunakgarments.repos.ProductsRepository
+//import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.launch
+//
+//class MainFragmentViewModel : ViewModel() {
+//    val products = MutableLiveData<List<Product>>()
+//
+//    fun setup() {
+//        viewModelScope.launch(Dispatchers.Default) {
+//            products.postValue(ProductsRepository().fetchAllProductsRetrofit())
+//        }
+//    }
+//
+//    fun search(term: String) {
+//        viewModelScope.launch(Dispatchers.Default) {
+//            products.postValue(ProductsRepository().searchForProducts(term))
+//        }
+//    }
+//
+//}
