3da7e08d4e5c47a816489d49f8c06c25ad23d009
= <anu71195@gmail.com>
2020-07-20 22:47:19 +0530

    Storage gradle updated and FirebaseUtil updated with connectStorage function.

--- app/src/main/java/com/raunakgarments/FirebaseUtil.kt
+++ app/src/main/java/com/raunakgarments/FirebaseUtil.kt
@@ -2,16 +2,24 @@ package com.raunakgarments
 
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class FirebaseUtil {
     lateinit var mFirebaseDatabase: FirebaseDatabase
     lateinit var mDatabaseReference: DatabaseReference
+    var mStorage: FirebaseStorage = FirebaseStorage.getInstance()
+    var mStorageRef: StorageReference = mStorage.reference
 
     constructor() {}
 
     public fun openFbReference(ref: String) {
         mFirebaseDatabase = FirebaseDatabase.getInstance()
-        mDatabaseReference = mFirebaseDatabase.getReference().child(ref)
-
+        mDatabaseReference = mFirebaseDatabase.reference.child(ref)
+        connectStorage()
+    }
+    private fun connectStorage() {
+        mStorage = FirebaseStorage.getInstance()
+        mStorageRef = mStorage.reference.child("deals_pictures")
     }
 }
