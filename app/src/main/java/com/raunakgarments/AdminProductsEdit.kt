17880607f51c7d88b2fd4b569653add6efba4fb1
= <anu71195@gmail.com>
2020-07-23 22:27:48 +0530

    Changed name from scroll view to app name.

--- app/src/main/java/com/raunakgarments/AdminProductsEdit.kt
+++ app/src/main/java/com/raunakgarments/AdminProductsEdit.kt
@@ -12,7 +12,7 @@ class AdminProductsEdit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_products_edit)
         setSupportActionBar(findViewById(R.id.activity_admin_products_edit_toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.activity_admin_products_edit_toolbar_layout).title = title
+//        findViewById<CollapsingToolbarLayout>(R.id.activity_admin_products_edit_toolbar_layout).title = title
 //        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
