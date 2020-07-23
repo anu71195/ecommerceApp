7242753813eb6ecf2e1f064b79da79c32d0dfd61
= <anu71195@gmail.com>
2020-07-23 22:11:18 +0530

    Code Refactoring.

--- app/src/main/java/com/raunakgarments/AdminProductsEdit.kt
+++ app/src/main/java/com/raunakgarments/AdminProductsEdit.kt
@@ -11,8 +11,8 @@ class AdminProductsEdit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_products_edit)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        setSupportActionBar(findViewById(R.id.activity_admin_products_edit_toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.activity_admin_products_edit_toolbar_layout).title = title
 //        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
