793ca2a518cffbeb606db3033a4a2298cd8d4d27
= <anu71195@gmail.com>
2020-07-25 12:34:08 +0530

    Setting up Toolbar in Cart Activity.

--- app/src/main/java/com/raunakgarments/UserCartActivity.kt
+++ app/src/main/java/com/raunakgarments/UserCartActivity.kt
@@ -12,10 +12,10 @@ class UserCartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_cart)
         setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
 }
