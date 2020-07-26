522b048af75acecee013b6713fa1e97fcd11f434
= <anu71195@gmail.com>
2020-07-26 23:32:56 +0530

    Constraint setup for scroll view in checkout activity screen.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -11,11 +11,11 @@ class CheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setSupportActionBar(findViewById(R.id.activity_checkout_toolbar))
+//        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
 }
