11c0e84881965971f1a4519cfd6f7da06b72e7c5
= <anu71195@gmail.com>
2020-07-27 09:46:32 +0530

    Added back button.

--- app/src/main/java/com/raunakgarments/ProfileActivity.kt
+++ app/src/main/java/com/raunakgarments/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.raunakgarments
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -12,10 +13,18 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         setSupportActionBar(findViewById(R.id.activity_profile_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        }
 //        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
 //        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
 //        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
