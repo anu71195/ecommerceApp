bb502827e790706515598739d78d92b2351b8c63
= <anu71195@gmail.com>
2020-07-27 17:06:35 +0530

    Automatically get email address on user profile and made it disable to edit.

--- app/src/main/java/com/raunakgarments/ProfileActivity.kt
+++ app/src/main/java/com/raunakgarments/ProfileActivity.kt
@@ -6,8 +6,17 @@ import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_profile_content_scrolling.*
+import org.jetbrains.anko.email
+import org.jetbrains.anko.image
+import java.io.File
 
 class ProfileActivity : AppCompatActivity() {
+    private var mFirebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var userEmailAddress: String = ""
+    private var emailVerified = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,15 @@ class ProfileActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
         }
+
+        userEmailAddress = mFirebaseAuth.currentUser?.email.toString()
+        emailVerified = mFirebaseAuth.currentUser?.isEmailVerified!!
+
+        activity_profile_content_scrolling_emailAddress.setText(userEmailAddress)
+        Picasso.get().load(R.drawable.ic_baseline_check_24_green).into(activity_profile_content_scrolling_emailVerification)
+
+//        ic_baseline_check_24_green
+
 //        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
 //        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
