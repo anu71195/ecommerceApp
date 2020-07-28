b3284c8f72da7050861526aa149ba2f5bd185de0
= <anu71195@gmail.com>
2020-07-28 12:53:26 +0530

    Setup constraints on contact us screen.

---
+++ app/src/main/java/com/raunakgarments/ContactUsActivity.kt
@@ -0,0 +1,16 @@
+package com.raunakgarments
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactUsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+        setSupportActionBar(findViewById(R.id.activity_contact_us_toolbar))
+    }
+}
