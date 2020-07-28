ef8f7577cd7791cda115ae9012a69d024715615d
= <anu71195@gmail.com>
2020-07-28 13:18:23 +0530

    Added workable back button on functions screen.

--- app/src/main/java/com/raunakgarments/AdminFunctionsActivity.kt
+++ app/src/main/java/com/raunakgarments/AdminFunctionsActivity.kt
@@ -1,6 +1,7 @@
 package com.raunakgarments
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -12,5 +13,13 @@ class AdminFunctionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_functions)
         setSupportActionBar(findViewById(R.id.activity_admin_functions_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
