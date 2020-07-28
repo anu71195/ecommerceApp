8f71911af99e07bd1bb1eb5a9d8e6263c5111cf2
= <anu71195@gmail.com>
2020-07-29 03:39:43 +0530

    Added back button non workable on edit contact screen.

--- app/src/main/java/com/raunakgarments/AdminContactUsEditActivity.kt
+++ app/src/main/java/com/raunakgarments/AdminContactUsEditActivity.kt
@@ -1,10 +1,13 @@
 package com.raunakgarments
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_admin_functions_content_scrolling.*
 
 class AdminContactUsEditActivity : AppCompatActivity() {
 
@@ -12,5 +15,13 @@ class AdminContactUsEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_contact_us_edit)
         setSupportActionBar(findViewById(R.id.activity_admin_contact_us_edit_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
