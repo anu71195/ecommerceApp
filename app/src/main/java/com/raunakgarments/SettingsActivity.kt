f9dafddd75522b4280214279cd6422b9b7c6e31d
= <anu71195@gmail.com>
2020-07-28 12:44:11 +0530

    Setup constraints on settings screen.

---
+++ app/src/main/java/com/raunakgarments/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.raunakgarments
+
+import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.raunakgarments.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.activity_settings_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
