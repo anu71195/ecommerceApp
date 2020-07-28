6d4e833ab75884137e0c4cb0d31f62e1c00564db
= <anu71195@gmail.com>
2020-07-29 04:05:37 +0530

    Code Refactoring.

--- app/src/main/java/com/raunakgarments/model/ContactUs.kt
+++ app/src/main/java/com/raunakgarments/model/ContactUs.kt
@@ -1,6 +1,12 @@
 package com.raunakgarments.model
 
 class ContactUs {
-    var phoneNumber = ""
-    var emailAddress = ""
+    var phoneNumber = "Not Available"
+    var emailAddress = "Not Available"
+
+    constructor()
+    constructor(phoneNumber: String, emailAddress: String) {
+        this.phoneNumber = phoneNumber
+        this.emailAddress = emailAddress
+    }
 }
