ae1c7a7045248f31ab2f74576646e01591256804
= <anu71195@gmail.com>
2020-07-29 03:28:49 +0530

    Intent to scrollable activity on click of contact us button on admin flow.

--- app/src/main/java/com/raunakgarments/AdminFunctionsActivity.kt
+++ app/src/main/java/com/raunakgarments/AdminFunctionsActivity.kt
@@ -1,11 +1,13 @@
 package com.raunakgarments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_admin_functions_content_scrolling.*
 
 class AdminFunctionsActivity : AppCompatActivity() {
 
@@ -16,6 +18,11 @@ class AdminFunctionsActivity : AppCompatActivity() {
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        }
+
+        Activity_admin_functions_content_scrolling_edit_contactUsButton.setOnClickListener {
+            var intent = Intent(this, AdminContactUsEditActivity::class.java)
+            this.startActivity(intent)
         }
     }
 
