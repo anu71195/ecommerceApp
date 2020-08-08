d45a3d57c8bc5bac9c78b722bbf1c0ae4b8c1f22
= <anu71195@gmail.com>
2020-08-08 18:49:25 +0530

    Null Value check on pinCode Edit Text.

--- app/src/main/java/com/raunakgarments/AdminPinCodeEditActivity.kt
+++ app/src/main/java/com/raunakgarments/AdminPinCodeEditActivity.kt
@@ -1,10 +1,12 @@
 package com.raunakgarments
 
 import android.os.Bundle
+import android.util.Log.d
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_admin_pin_code_edit_content_scrolling.*
 
 class AdminPinCodeEditActivity : AppCompatActivity() {
 
@@ -12,5 +14,15 @@ class AdminPinCodeEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_pin_code_edit)
         setSupportActionBar(findViewById(R.id.activity_admin_pin_code_edit_toolbar))
+
+        var firebaseUtil = FirebaseUtil()
+        firebaseUtil.openFbReference("pincode")
+
+        activity_admin_pin_code_edit_content_scrolling_pinCodeAddButton.setOnClickListener {
+            var pinCodeText = activity_admin_pin_code_edit_content_scrolling_pincodeEditView.text.toString()
+            if(pinCodeText != "") {
+                d("anurag", "Pin Code Text")
+            }
+        }
     }
 }
