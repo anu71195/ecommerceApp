a86a8f926ca09f83a7e14ebbf19d0b5ec1fef8da
= <anu71195@gmail.com>
2020-08-15 20:06:03 +0530

    Code Refactoring.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -31,15 +31,15 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
         activity_checkout_content_scrolling_payButton.setOnClickListener {
             val profile =
                 Gson().fromJson<Profile>(intent.getStringExtra("profile"), Profile::class.java)
-            startPayment(profile)
+            val userID = intent.getStringExtra("userID")
+            startPayment(profile, userID)
         }
     }
 
-    private fun startPayment(profile: Profile) {
+    private fun startPayment(profile: Profile, userID: String) {
         /*
         *  You need to pass current activity in order to let Razorpay create CheckoutActivity
         * */
-        val activity: Activity = this
         val co = Checkout()
 
         var firebaseUtil = FirebaseUtil()
@@ -50,33 +50,40 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     d("razorPay", "${snapshot.toString()}")
                     co.setKeyID(snapshot.value.toString())
-                    try {
-                        val options = JSONObject()
-                        options.put("name", "Raunak Garments")
-                        options.put("description", "Net order Charges")
-                        //You can omit the image option to fetch the image from dashboard
-                        options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
-                        options.put("theme.color", "#3399cc");
-                        options.put("currency", "INR");
-//            options.put("order_id", "order_DBJOWzybf0sJbb");
-                        options.put("amount", "50000")//pass amount in currency subunits
-
-                        val prefill = JSONObject()
-                        prefill.put("email", profile.email)
-                        prefill.put("contact", profile.number)
-
-                        options.put("prefill", prefill)
-                        co.open(activity, options)
-                    } catch (e: Exception) {
-                        Toast.makeText(
-                            activity,
-                            "Error in payment: " + e.message,
-                            Toast.LENGTH_LONG
-                        ).show()
-                        e.printStackTrace()
-                    }
+                    startRazorpayPayment(co, profile, userID)
                 }
             })
+    }
+
+    fun startRazorpayPayment(co: Checkout, profile: Profile, userID: String) {
+        val activity: Activity = this
+        try {
+            val options = JSONObject()
+            val orderID = userID + System.currentTimeMillis().toString()
+            d("orderID", orderID)
+            options.put("name", "Raunak Garments")
+            options.put("description", "Net order Charges")
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
+            options.put("theme.color", "#3399cc")
+            options.put("currency", "INR")
+//                        options.put("order_id", orderID)
+            options.put("amount", "50000")//pass amount in currency subunits
+
+            val prefill = JSONObject()
+            prefill.put("email", profile.email)
+            prefill.put("contact", profile.number)
+
+            options.put("prefill", prefill)
+            co.open(activity, options)
+        } catch (e: Exception) {
+            Toast.makeText(
+                activity,
+                "Error in payment: " + e.message,
+                Toast.LENGTH_LONG
+            ).show()
+            e.printStackTrace()
+        }
     }
 
 
