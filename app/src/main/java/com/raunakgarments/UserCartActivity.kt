6c2a0ac35937f1ae3f7239ed8e8324510e693eb9
= <anu71195@gmail.com>
2020-08-30 00:00:01 +0530

    Code Refactoring.

--- app/src/main/java/com/raunakgarments/UserCartActivity.kt
+++ app/src/main/java/com/raunakgarments/UserCartActivity.kt
@@ -1,31 +1,21 @@
-package com.raunakgarments
+    package com.raunakgarments
 
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.replace
-import kotlinx.android.synthetic.main.activity_user_cart.*
+    import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
+    import kotlinx.android.synthetic.main.activity_user_cart.*
 
-class UserCartActivity : AppCompatActivity() {
+    class UserCartActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_cart)
-        setSupportActionBar(activity_user_cart_toolbar)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_user_cart)
+            setSupportActionBar(activity_user_cart_toolbar)
+            supportActionBar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_user_cart_frameLayout, UserCartActivityrvFragment(this)).commit()
         }
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.activity_user_cart_frameLayout, UserCartActivityrvFragment(this)).commit()
     }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-    }
-}
