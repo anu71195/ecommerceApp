b3d2ff4d85fd1b8efdcd86468b6325925627e962
= <anu71195@gmail.com>
2020-08-31 04:49:47 +0530

    Removed stock from product.

--- app/src/main/java/com/raunakgarments/model/Product.kt
+++ app/src/main/java/com/raunakgarments/model/Product.kt
@@ -9,7 +9,6 @@ class Product() {
     var price = 0.0
     var photoUrl: String = ""
     var description: String = ""
-    var stock = 0
     var tagArray: HashMap<String, Int> = HashMap<String, Int>()
 
     fun populate(title: String, price: Double, photoUrl: String, description: String) {
