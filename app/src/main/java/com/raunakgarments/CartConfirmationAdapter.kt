3c111e97b2338da78a2a8c5888bb8517bbc867da
= <anu71195@gmail.com>
2020-09-14 22:26:23 +0530

    Added other variable for dummy data in cart confirmation recycler view.

--- app/src/main/java/com/raunakgarments/CartConfirmationAdapter.kt
+++ app/src/main/java/com/raunakgarments/CartConfirmationAdapter.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import org.w3c.dom.Text
 
 class CartConfirmationAdapter : RecyclerView.Adapter<CartConfirmationAdapter.DealViewHolder>() {
     class DealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView = itemView.findViewById(R.id.cart_confirmation_product_row_title)
         var image: ImageView = itemView.findViewById(R.id.cart_confirmation_product_row_photo)
+        var quantity: TextView = itemView.findViewById(R.id.cart_confirmation_product_row_quantity)
+        var price: TextView = itemView.findViewById(R.id.cart_confirmation_product_row_price)
     }
 
     override fun onCreateViewHolder(
@@ -24,7 +29,12 @@ class CartConfirmationAdapter : RecyclerView.Adapter<CartConfirmationAdapter.Dea
     }
 
     override fun onBindViewHolder(holder: CartConfirmationAdapter.DealViewHolder, position: Int) {
-        Picasso.get().load("https://images-na.ssl-images-amazon.com/images/I/61Ca1VD6fyL._UL1024_.jpg").into(holder.image)
+        holder.title.text = "Colorful Tshirt"
+        Picasso.get()
+            .load("https://images-na.ssl-images-amazon.com/images/I/61Ca1VD6fyL._UL1024_.jpg")
+            .into(holder.image)
+        holder.quantity.text = "3"
+        holder.price.text = "₹" + "320" + " X " + "3" + " = ₹" + "960"
     }
 
     override fun getItemCount(): Int {
