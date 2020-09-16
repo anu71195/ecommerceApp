eea1e81597c4451887966728999437a545435208
= <anu71195@gmail.com>
2020-09-16 20:02:49 +0530

    Passed total cart value to razorpay.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -10,6 +10,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.gson.Gson
+import com.raunakgarments.helper.CostFormatterHelper
 import com.raunakgarments.model.Profile
 import com.razorpay.Checkout
 import com.razorpay.PaymentResultListener
@@ -74,33 +75,37 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
 
     fun startRazorpayPayment(co: Checkout, profile: Profile, userID: String) {
         val activity: Activity = this
-        try {
-            val options = JSONObject()
-            val orderID = "ERUS" + userID + "EMTI" + System.currentTimeMillis()
-                .toString() + "RDEOR" + profile.orderNumber.toString()
-            d("orderID", orderID)
-            options.put("name", "Raunak Garments")
-            options.put("description", "Net order Charges")
-            //You can omit the image option to fetch the image from dashboard
-            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
-            options.put("theme.color", "#3399cc")
-            options.put("currency", "INR")
+        var totalCartCost = intent.getDoubleExtra("totalCartCost", 0.0)
+        if (totalCartCost > 0.0) {
+            totalCartCost = CostFormatterHelper().formatCost(totalCartCost*100)
+            try {
+                val options = JSONObject()
+                val orderID = "ERUS" + userID + "EMTI" + System.currentTimeMillis()
+                    .toString() + "RDEOR" + profile.orderNumber.toString()
+                d("orderID", orderID)
+                options.put("name", "Raunak Garments")
+                options.put("description", "Net order Charges")
+                //You can omit the image option to fetch the image from dashboard
+                options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
+                options.put("theme.color", "#3399cc")
+                options.put("currency", "INR")
 //                        options.put("order_id", orderID)
-            options.put("amount", "50000")//pass amount in currency subunits
+                options.put("amount", totalCartCost.toInt().toString())//pass amount in currency subunits
 
-            val prefill = JSONObject()
-            prefill.put("email", profile.email)
-            prefill.put("contact", profile.number)
+                val prefill = JSONObject()
+                prefill.put("email", profile.email)
+                prefill.put("contact", profile.number)
 
-            options.put("prefill", prefill)
-            co.open(activity, options)
-        } catch (e: Exception) {
-            Toast.makeText(
-                activity,
-                "Error in payment: " + e.message,
-                Toast.LENGTH_LONG
-            ).show()
-            e.printStackTrace()
+                options.put("prefill", prefill)
+                co.open(activity, options)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    activity,
+                    "Error in payment: " + e.message,
+                    Toast.LENGTH_LONG
+                ).show()
+                e.printStackTrace()
+            }
         }
     }
 
