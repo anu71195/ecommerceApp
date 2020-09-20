872085df160ac7c11194d92741b82b2747f172d1
= <anu71195@gmail.com>
2020-09-20 22:45:42 +0530

    Todo added.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -93,6 +93,7 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
             })
     }
     /*todo 10 min if not paid then pay with razor pay button get disabled*/
+    /*todo record the product while tking locks as well*/
     /*todo release lock when */
     fun startRazorpayPayment(co: Checkout, profile: Profile, userID: String) {
         val activity: Activity = this
