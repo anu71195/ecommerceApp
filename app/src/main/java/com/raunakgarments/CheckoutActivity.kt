57b3608d3d4fa364162464552ced527d0766d7b3
= <anu71195@gmail.com>
2020-09-20 19:39:58 +0530

    Another todo.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -115,6 +115,7 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
     }
     /*todo decrement when product payment is done*/
     /*todo keep another check on usercart for boughtticket*/
+    /*todo release lock if timeout is not done*/
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
     }
