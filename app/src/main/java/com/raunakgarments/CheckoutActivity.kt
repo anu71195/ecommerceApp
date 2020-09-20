db640b62fdbba6ef40145bebd81ea89284ab6aba
= <anu71195@gmail.com>
2020-09-21 02:17:06 +0530

    Todo for timeout toast done.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -101,7 +101,6 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
     /*todo 10 min if not paid then pay with razor pay button get disabled*/
     /*todo record the product while tking locks as well*/
     /*todo release lock when */
-    /*todo time out toast*/
     /*todo after successful payment please wait not close */
     fun startRazorpayPayment(co: Checkout, profile: Profile, userID: String) {
         val activity: Activity = this
