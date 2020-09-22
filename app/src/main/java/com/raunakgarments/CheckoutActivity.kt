4a0293ed88fee4d4a0ba5c01111aba197e88fe2b
= <anu71195@gmail.com>
2020-09-22 07:48:44 +0530

    Added a todo.

--- app/src/main/java/com/raunakgarments/CheckoutActivity.kt
+++ app/src/main/java/com/raunakgarments/CheckoutActivity.kt
@@ -158,6 +158,7 @@ class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
 
     /*todo decrement when product payment is done*/
     /*todo release lock if timeout is not done*/
+    /*todo add timeout in the background while razorpay is up and increment user product lock timeout accordingly*/
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
     }
