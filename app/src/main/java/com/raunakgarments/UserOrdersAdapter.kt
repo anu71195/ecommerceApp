9bf4c29363e76cc0220dc4b3acb91641b3e02a4c
= <anu71195@gmail.com>
2020-10-03 11:11:26 +0530

    Showing total cost, delivery status and order status in recycler view in user orders screen.

--- app/src/main/java/com/raunakgarments/UserOrdersAdapter.kt
+++ app/src/main/java/com/raunakgarments/UserOrdersAdapter.kt
@@ -16,22 +16,22 @@ import com.google.gson.Gson
 import com.raunakgarments.model.UserOrders
 import org.w3c.dom.Text
 
-class UserOrdersAdapter: RecyclerView.Adapter<UserOrdersAdapter.UserOrderViewHolder>() {
+class UserOrdersAdapter : RecyclerView.Adapter<UserOrdersAdapter.UserOrderViewHolder>() {
 
     var userOrdersList: MutableList<UserOrders> = ArrayList()
 
     fun populate(userOrdersRef: String, userOrdersActivity: UserOrdersActivity) {
         var userOrderFirebaseUtil = FirebaseUtil()
-        userOrderFirebaseUtil.openFbReference(userOrdersRef+"/"+FirebaseAuth.getInstance().uid)
+        userOrderFirebaseUtil.openFbReference(userOrdersRef + "/" + FirebaseAuth.getInstance().uid)
 
-        userOrderFirebaseUtil.mDatabaseReference.addChildEventListener(object : ChildEventListener{
+        userOrderFirebaseUtil.mDatabaseReference.addChildEventListener(object : ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                if(snapshot.exists()) {
+                if (snapshot.exists()) {
                     var userOrders = snapshot.getValue(UserOrders::class.java)
                     d("UserOrdersAdapter", "populate-${Gson().toJson(userOrders)}")
                     if (userOrders != null) {
                         userOrdersList.add(userOrders)
-                        notifyItemInserted(userOrdersList.size-1)
+                        notifyItemInserted(userOrdersList.size - 1)
                     } else {
                         d("UserOrdersAdapter", "populate-userOrders is null")
                     }
@@ -39,6 +39,7 @@ class UserOrdersAdapter: RecyclerView.Adapter<UserOrdersAdapter.UserOrderViewHol
                     d("UserOrdersAdapter", "populate-snapshot does not exist")
                 }
             }
+
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
             override fun onChildRemoved(snapshot: DataSnapshot) {}
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
@@ -48,18 +49,22 @@ class UserOrdersAdapter: RecyclerView.Adapter<UserOrdersAdapter.UserOrderViewHol
     }
 
     class UserOrderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var titleButton: Button = itemView.findViewById(R.id.activity_user_orders_adapter_user_orders_row_button)
-        var informationTextView: TextView = itemView.findViewById(R.id.activity_user_orders_adapter_user_orders_row_textViewInformation)
+        var titleButton: Button =
+            itemView.findViewById(R.id.activity_user_orders_adapter_user_orders_row_button)
+        var informationTextView: TextView =
+            itemView.findViewById(R.id.activity_user_orders_adapter_user_orders_row_textViewInformation)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserOrderViewHolder {
-        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_user_orders_adapter_user_orders_row, parent, false)
+        var itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_user_orders_adapter_user_orders_row, parent, false)
         return UserOrderViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: UserOrderViewHolder, position: Int) {
         holder.titleButton.text = userOrdersList[position].dateStamp
-        holder.informationTextView.text = userOrdersList[position].totalCost
+        holder.informationTextView.text =
+            "total Cost = " + userOrdersList[position].totalCost + "\n" + "delivery status = " + userOrdersList[position].deliveryStatus + "\n" + "orderStatus = " + userOrdersList[position].orderStatus
     }
 
     override fun getItemCount(): Int {
