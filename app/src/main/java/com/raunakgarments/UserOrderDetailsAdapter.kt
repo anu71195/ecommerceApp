daea143961a3ce956b6d3cfc379ff1abb930bea1
= <anu71195@gmail.com>
2020-10-05 02:14:23 +0530

    Populating data in recyclerview by number of unique items ordered and their title.

--- app/src/main/java/com/raunakgarments/UserOrderDetailsAdapter.kt
+++ app/src/main/java/com/raunakgarments/UserOrderDetailsAdapter.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import com.raunakgarments.model.ConfirmationCartProduct
+import com.raunakgarments.model.Product
 import com.raunakgarments.model.UserOrders
 
 class UserOrderDetailsAdapter :
@@ -15,14 +17,22 @@ class UserOrderDetailsAdapter :
 
     private lateinit var activityIntent: Intent
     private lateinit var userOrders: UserOrders
+    var productList: MutableList<ConfirmationCartProduct> = ArrayList()
 
     fun populate(intent: Intent) {
         this.activityIntent = intent
         this.userOrders =
             Gson().fromJson<UserOrders>(intent.getStringExtra("userOrders"), UserOrders::class.java)
+        for (orderedProduct in userOrders.orders) {
+            productList.add(orderedProduct.value)
+        }
         Log.d(
             "UserOrderDetailsAdapter",
             "populate-${Gson().toJson(userOrders)}"
+        )
+        Log.d(
+            "UserOrderDetailsAdapter",
+            "populate-${Gson().toJson(productList)}"
         )
 
     }
@@ -45,10 +55,10 @@ class UserOrderDetailsAdapter :
         holder: UserOrderDetailsViewHolder,
         position: Int
     ) {
-        holder.productTitle.text = "Hello World ${position}"
+        holder.productTitle.text = productList[position].title
     }
 
     override fun getItemCount(): Int {
-        return 100
+        return productList.size
     }
 }
