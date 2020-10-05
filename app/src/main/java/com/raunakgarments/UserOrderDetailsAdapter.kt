08ab2df86eb587c02ec87045e18f6d7cbfa26f38
= <anu71195@gmail.com>
2020-10-05 09:18:38 +0530

    Todo for toast on on add to cart is handled and done.

--- app/src/main/java/com/raunakgarments/UserOrderDetailsAdapter.kt
+++ app/src/main/java/com/raunakgarments/UserOrderDetailsAdapter.kt
@@ -59,7 +59,6 @@ class UserOrderDetailsAdapter :
             itemView.findViewById(R.id.activity_user_order_details_adapter_user_orders_row_productOrderStatusTextView)
     }
 
-    //todo you can manage product from cart do something about that
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
