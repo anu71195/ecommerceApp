cb350e24098c8c959c5725441e46ca93216e40f0
= <anu71195@gmail.com>
2020-10-06 07:46:27 +0530

    Added switch for show unavailable products in SettingsActivity.kt.

--- app/src/main/java/com/raunakgarments/SettingsActivity.kt
+++ app/src/main/java/com/raunakgarments/SettingsActivity.kt
@@ -1,12 +1,14 @@
 package com.raunakgarments
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.MenuItem
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.raunakgarments.R
+import kotlinx.android.synthetic.main.activity_settings_content_scrolling.*
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -18,9 +20,18 @@ class SettingsActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
         }
+
+        activity_settings_content_scrolling_UnavailableSwitch.layoutParams.width =
+            getScreenWidth() / 3
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun getScreenWidth(): Int {
+        val displayMetrics = DisplayMetrics()
+        this.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.widthPixels
     }
 }
