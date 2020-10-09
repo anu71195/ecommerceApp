2ed4a2c77c16831e0316039afde9811b1f020808
= <anu71195@gmail.com>
2020-10-09 18:44:50 +0530

    Data is being retrieved for user orders in admin flow.

--- app/src/main/java/com/raunakgarments/admin/AdminOrdersAdapter.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminOrdersAdapter.kt
@@ -1,15 +1,46 @@
 package com.raunakgarments.admin
 
+import android.app.Activity
+import android.util.Log
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.raunakgarments.R
+import com.raunakgarments.helper.FirebaseUtil
 
 class AdminOrdersAdapter : RecyclerView.Adapter<AdminOrdersAdapter.AdminOrderViewHolder>()  {
 
-    fun populate(string: String, adminOrdersActivity: AdminOrdersActivity) {
+    private lateinit var adminOrdersActivity: Activity
+
+    fun populate(userOrdersRef: String, adminOrdersActivity: AdminOrdersActivity) {
+        this.adminOrdersActivity = adminOrdersActivity
+
+        var adminOrderFirebaseUtil = FirebaseUtil()
+        adminOrderFirebaseUtil.openFbReference(userOrdersRef)
+
+        adminOrderFirebaseUtil.mDatabaseReference.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                if(snapshot.exists()) {
+                    d("AdminOrdersAdapter", "populate-${snapshot.key}")
+                    d("AdminOrdersAdapter", "populate-${snapshot.value}")
+                } else {
+                    d("AdminOrdersAdapter", "populate-snapshot does not exist")
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onChildRemoved(snapshot: DataSnapshot) { }
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
 
     }
 
