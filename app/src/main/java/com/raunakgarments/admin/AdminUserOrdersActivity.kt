1f1aaf135b5cab5997334a2ed3547a47feebacbb
= <anu71195@gmail.com>
2020-10-10 10:31:40 +0530

    renamed occurences of activity_admin_user_order to activity_admin_user_orders.

--- app/src/main/java/com/raunakgarments/admin/AdminUserOrdersActivity.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrdersActivity.kt
@@ -12,7 +12,7 @@ class AdminUserOrdersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_user_orders)
-        setSupportActionBar(findViewById(R.id.activity_admin_user_order_toolbar))
+        setSupportActionBar(findViewById(R.id.activity_admin_user_orders_toolbar))
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
