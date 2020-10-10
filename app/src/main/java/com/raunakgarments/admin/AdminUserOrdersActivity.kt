3fb55ff681a7a248e29b07fc30e78db70299770f
= <anu71195@gmail.com>
2020-10-10 10:28:35 +0530

    Renamed activity_admin_user_order.xml to activity_admin_user_orders.xml.

--- app/src/main/java/com/raunakgarments/admin/AdminUserOrdersActivity.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrdersActivity.kt
@@ -2,20 +2,16 @@ package com.raunakgarments.admin
 
 import android.os.Bundle
 import android.util.Log.d
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.raunakgarments.R
 import com.raunakgarments.model.UserOrderProfile
-import com.raunakgarments.model.UserOrders
 
 class AdminUserOrdersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_user_order)
+        setContentView(R.layout.activity_admin_user_orders)
         setSupportActionBar(findViewById(R.id.activity_admin_user_order_toolbar))
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
