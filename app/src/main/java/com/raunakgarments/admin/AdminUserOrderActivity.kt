fb9d1ece217123b4400474640f3814a1b86e4668
= <anu71195@gmail.com>
2020-10-10 10:05:52 +0530

    User Profile is successfully retrieved in AdminUserOrderActivity.kt.

--- app/src/main/java/com/raunakgarments/admin/AdminUserOrderActivity.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrderActivity.kt
@@ -1,11 +1,15 @@
 package com.raunakgarments.admin
 
 import android.os.Bundle
+import android.util.Log.d
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.raunakgarments.R
+import com.raunakgarments.model.UserOrderProfile
+import com.raunakgarments.model.UserOrders
 
 class AdminUserOrderActivity : AppCompatActivity() {
 
@@ -17,5 +21,13 @@ class AdminUserOrderActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
         }
+        initializeAdminUserOrdersRecyclerViewAdapter()
+    }
+
+    private fun initializeAdminUserOrdersRecyclerViewAdapter() {
+        var adminUserOrders =
+            Gson().fromJson<UserOrderProfile>(intent.getStringExtra("userOrderProfile"), UserOrderProfile::class.java)
+        d("AdminUserOrderActivity", "initializeAdminUserOrdersRecyclerViewAdapter - ${adminUserOrders}")
+        d("AdminUserOrderActivity", "initializeAdminUserOrdersRecyclerViewAdapter - ${Gson().toJson(adminUserOrders)}")
     }
 }
