1ebd7bdce8d83570fc06bbbbca6a65170eedd469
= <anu71195@gmail.com>
2020-10-10 09:54:04 +0530

    Added activity to show user orders in admin flow for specific users.

---
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrderActivity.kt
@@ -0,0 +1,21 @@
+package com.raunakgarments.admin
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.raunakgarments.R
+
+class AdminUserOrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_user_order)
+        setSupportActionBar(findViewById(R.id.activity_admin_user_order_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
+        }
+    }
+}
