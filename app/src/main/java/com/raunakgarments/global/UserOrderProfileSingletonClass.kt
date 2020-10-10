15eb5d1a816321ce8bd7e98e17f1743ae4d58ff4
= <anu71195@gmail.com>
2020-10-10 15:21:24 +0530

    Added object class for UserOrderProfileSingletonClass.kt.

---
+++ app/src/main/java/com/raunakgarments/global/UserOrderProfileSingletonClass.kt
@@ -0,0 +1,7 @@
+package com.raunakgarments.global
+
+import com.raunakgarments.model.UserOrderProfile
+
+object UserOrderProfileSingletonClass {
+    lateinit var userOrderProfile: UserOrderProfile
+}
