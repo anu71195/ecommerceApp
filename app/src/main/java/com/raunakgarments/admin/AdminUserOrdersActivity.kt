890e305d62290d73d8f0ca69df2d6460b69e1dd4
= <anu71195@gmail.com>
2020-10-10 13:21:51 +0530

    UserProfile is retrieved in AdminUserOrdersAdapter.kt and passed from AdminUserOrdersActivity.kt.

--- app/src/main/java/com/raunakgarments/admin/AdminUserOrdersActivity.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrdersActivity.kt
@@ -1,16 +1,10 @@
 package com.raunakgarments.admin
 
 import android.os.Bundle
-import android.util.Log.d
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import com.google.gson.Gson
 import com.raunakgarments.R
-import com.raunakgarments.UserOrdersAdapter
-import com.raunakgarments.model.UserOrderProfile
 import kotlinx.android.synthetic.main.activity_admin_user_orders_content_scrolling.*
-import kotlinx.android.synthetic.main.activity_user_orders_content_scrolling.*
-import kotlinx.android.synthetic.main.activity_user_orders_content_scrolling.activity_user_orders_content_scrolling_OrdersRecyclerView
 
 class AdminUserOrdersActivity : AppCompatActivity() {
 
@@ -28,8 +22,8 @@ class AdminUserOrdersActivity : AppCompatActivity() {
     private fun initializeAdminUserOrdersRecyclerViewAdapter() {
         val adminUserOrdersAdapter = AdminUserOrdersAdapter()
         val productsLayoutManager = GridLayoutManager(this, 1)
-//        productsLayoutManager.reverseLayout = true
-//        adminUserOrdersAdapter.populate(getString(R.string.database_userOrders), this)
+        productsLayoutManager.reverseLayout = true
+        adminUserOrdersAdapter.populate(getString(R.string.database_userOrders), intent, this)
         activity_admin_user_orders_content_scrolling_OrdersRecyclerView.adapter = adminUserOrdersAdapter
         activity_admin_user_orders_content_scrolling_OrdersRecyclerView.layoutManager = productsLayoutManager
     }
--- app/src/main/java/com/raunakgarments/admin/AdminUserOrdersAdapter.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrdersAdapter.kt
@@ -1,15 +1,43 @@
 package com.raunakgarments.admin
 
+import android.app.Activity
+import android.content.Intent
+import android.util.Log
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.gson.Gson
 import com.raunakgarments.R
-import com.raunakgarments.UserOrdersAdapter
+import com.raunakgarments.helper.FirebaseUtil
+import com.raunakgarments.model.UserOrderProfile
+import com.raunakgarments.model.UserOrders
 
 class AdminUserOrdersAdapter :
     RecyclerView.Adapter<AdminUserOrdersAdapter.AdminUserOrderViewHolder>() {
+
+    var userOrdersList: MutableList<UserOrders> = ArrayList()
+    private lateinit var adminUserOrdersActivity: Activity
+    private lateinit var userOrderProfile: UserOrderProfile
+
+    fun populate(
+        userOrdersRef: String,
+        intent: Intent,
+        adminUserOrdersActivity: AdminUserOrdersActivity
+    ) {
+        this.adminUserOrdersActivity = adminUserOrdersActivity
+        this.userOrderProfile =
+            Gson().fromJson<UserOrderProfile>(intent.getStringExtra("userOrderProfile"), UserOrderProfile::class.java)
+
+        d("AdminUserOrdersAdapter", "populate-${Gson().toJson(userOrderProfile)}")
+
+
+    }
 
     class AdminUserOrderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
         var title: TextView = itemView.findViewById(R.id.activity_user_orders_adapter_admin_user_orders_row_textViewInformation)
