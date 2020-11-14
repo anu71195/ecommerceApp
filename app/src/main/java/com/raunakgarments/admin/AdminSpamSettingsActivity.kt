df24981942024081a1a9b4c5e3688fe26b2c6407
= <anu71195@gmail.com>
2020-11-15 04:08:48 +0530

    Admin is able to update spam Settings.

--- app/src/main/java/com/raunakgarments/admin/AdminSpamSettingsActivity.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminSpamSettingsActivity.kt
@@ -1,11 +1,19 @@
 package com.raunakgarments.admin
 
 import android.os.Bundle
+import android.util.Log.d
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 import com.raunakgarments.R
+import com.raunakgarments.helper.FirebaseUtil
+import com.raunakgarments.model.SpamSettings
+import kotlinx.android.synthetic.main.activity_admin_spam_settings_content_scrolling.*
 
 class AdminSpamSettingsActivity : AppCompatActivity() {
 
@@ -18,5 +26,44 @@ class AdminSpamSettingsActivity : AppCompatActivity() {
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_white_24)
         }
 
+        populateSpamSettingsData()
+        updateSpamSettingsClickListener()
+    }
+
+    private fun populateSpamSettingsData() {
+        var spamSettingsFirebaseUtil = FirebaseUtil()
+        spamSettingsFirebaseUtil.openFbReference("spamSettings")
+
+        spamSettingsFirebaseUtil.mDatabaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var spamSettings = SpamSettings()
+                if(snapshot.exists()) {
+                    spamSettings = snapshot.getValue(SpamSettings::class.java)!!
+                } else  {
+                    d("AdminSpamSettingsActivity", "populateSPamSEttingsData - snapshot does not exist")
+                }
+                activity_admin_spam_settings_content_scrolling_attemptNumbersText.setText(spamSettings.lockLimit.toString())
+                activity_admin_spam_settings_content_scrolling_deletionDaysText.setText(spamSettings.daysDataAvailability.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+    }
+
+    private fun updateSpamSettingsClickListener() {
+        activity_admin_spam_settings_content_scrolling_updateSpamSettingsButton.setOnClickListener {
+
+            var spamSettingsFirebaseUtil = FirebaseUtil()
+            spamSettingsFirebaseUtil.openFbReference("spamSettings")
+
+            var spamSettings = SpamSettings()
+
+            spamSettings.lockLimit = activity_admin_spam_settings_content_scrolling_attemptNumbersText.text.toString().toInt()
+            spamSettings.daysDataAvailability = activity_admin_spam_settings_content_scrolling_deletionDaysText.text.toString().toInt()
+
+            spamSettingsFirebaseUtil.mDatabaseReference.setValue(spamSettings)
+
+        }
     }
 }
---
+++ app/src/main/java/com/raunakgarments/model/SpamSettings.kt
@@ -0,0 +1,7 @@
+package com.raunakgarments.model
+
+class SpamSettings {
+
+    var lockLimit = 1000000
+    var daysDataAvailability = 1000000
+}
