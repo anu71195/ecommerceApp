b466d4f8ed3ccf32a1e914a0ebc6495f19eded6d
= <anu71195@gmail.com>
2020-11-21 01:56:34 +0530

    Read access permission popup showing. Calling is left.

--- app/src/main/java/com/raunakgarments/admin/AdminFragment.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminFragment.kt
@@ -1,7 +1,9 @@
 package com.raunakgarments.admin
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -15,6 +17,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -43,6 +49,7 @@ class AdminFragment(productActivityNew: AdminProductActivityNew) : Fragment() {
     lateinit var mFirebaseDatebase: FirebaseDatabase
     lateinit var mDatabaseReference: DatabaseReference
     private val PICTURE_RESULT = 42
+    private val STORAGE_PERMISSION_CODE = 1
     lateinit var contextView: View
     var context = productActivityNew
 
@@ -155,6 +162,71 @@ class AdminFragment(productActivityNew: AdminProductActivityNew) : Fragment() {
                         Picasso.get().load(url).into(uploadedImagePreviewAdmin)
                     }
                 }
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+
+        if(ContextCompat.checkSelfPermission(context,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "You have already granted the permission", Toast.LENGTH_LONG)
+            d("AdminFragment", "checkPermissions - permission already granted")
+        } else {
+            d("AdminFragment", "checkPermissions - permission already not granted")
+                requestStoragePermission()
+
+        }
+    }
+
+private fun requestStoragePermission() {
+    if(ActivityCompat.shouldShowRequestPermissionRationale(context, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+        d("AdminFragment", "requestStoragePermission - showing popup")
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Permission needed")
+        builder.setMessage("This permission is needed because of this and that")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton("OK") { dialogInterface, which ->
+            ActivityCompat.requestPermissions(context,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+        }
+        builder.setNegativeButton("cancel") { dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    } else {
+        d("AdminFragment", "requestStoragePermission - not showing popup")
+        ActivityCompat.requestPermissions(context,arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+    }
+}
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            STORAGE_PERMISSION_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT)
+                    d("AdminFragment", "onRequestPermissionsResult - permission granted")
+                } else {
+                    Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT)
+                    d("AdminFragment", "onRequestPermissionsResult - permission denied")
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
             }
         }
     }
