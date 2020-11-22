709de415ef0cdfe1184dc704b31cf29a2181a3e7
= <anu71195@gmail.com>
2020-11-22 22:10:35 +0530

    Added summary in UserOrderDetailsActivity.kt.

--- app/src/main/java/com/raunakgarments/UserOrderDetailsActivity.kt
+++ app/src/main/java/com/raunakgarments/UserOrderDetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.raunakgarments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
@@ -23,6 +24,18 @@ class UserOrderDetailsActivity : AppCompatActivity() {
         this.userOrders =
             Gson().fromJson<UserOrders>(intent.getStringExtra("userOrders"), UserOrders::class.java)
         activity_user_order_details_content_scrolling_OrdersTotalCost.text = "Total Cost = ₹" + this.userOrders.totalCost
+        activity_user_order_details_content_scrolling_OrdersTotalItems.text = "Total Items = ${this.userOrders.orders.size}"
+        activity_user_order_details_content_scrolling_OrdersDeliveryStatus.text = "Delivery Status = ${this.userOrders.deliveryStatus}"
+        activity_user_order_details_content_scrolling_OrdersOrderStatus.text = "Order Status = ${this.userOrders.orderStatus}"
+
+        if(this.userOrders.deliveryStatus == "Delivered") {
+            activity_user_order_details_content_scrolling_OrdersDeliveryStatus.setBackgroundColor(Color.parseColor("#008000"))
+        }
+
+        if (this.userOrders.orderStatus == "Payment Done") {
+            activity_user_order_details_content_scrolling_OrdersOrderStatus.setBackgroundColor(Color.parseColor("#008000"))
+        }
+
         initializeUserOrdersRecyclerViewAdapter()
     }
 
