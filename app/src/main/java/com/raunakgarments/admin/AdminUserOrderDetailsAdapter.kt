60f53204e144711aaac4c2433bbd5ad35b3ea1b4
= <anu71195@gmail.com>
2020-11-28 04:23:04 +0530

    Change of order and delivery status on user details screen in admin flow on the products.

--- app/src/main/java/com/raunakgarments/admin/AdminUserOrderDetailsAdapter.kt
+++ app/src/main/java/com/raunakgarments/admin/AdminUserOrderDetailsAdapter.kt
@@ -15,12 +15,12 @@ import com.raunakgarments.global.AdminOrderSingletonClass
 import com.raunakgarments.global.OrderStatusObject
 import com.raunakgarments.model.UserOrderProduct
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_admin_user_order_details_content_scrolling.*
 
 class AdminUserOrderDetailsAdapter : RecyclerView.Adapter<AdminUserOrderDetailsAdapter.AdminUserOrderDetailsViewHolder>() {
 
     private lateinit var activityIntent: Intent
     var productList: MutableList<UserOrderProduct> = ArrayList()
+    var idPositionMap: HashMap<String, Int> = HashMap<String, Int>()
     private lateinit var adminUserOrderDetailsActivity: AdminUserOrderDetailsActivity
 
     fun populate(intent: Intent, userOrderDetailsActivity: AdminUserOrderDetailsActivity) {
@@ -29,6 +29,7 @@ class AdminUserOrderDetailsAdapter : RecyclerView.Adapter<AdminUserOrderDetailsA
 
         for (orderedProduct in AdminOrderSingletonClass.userOrders.orders) {
             productList.add(orderedProduct.value)
+            userOrderDetailsActivity.populateOrderDeliveryStatusSingletonList(AdminOrderSingletonClass.userOrders.orders)
         }
         Log.d(
             "UserOrderDetailsAdapter",
@@ -64,6 +65,9 @@ class AdminUserOrderDetailsAdapter : RecyclerView.Adapter<AdminUserOrderDetailsA
     }
 
     override fun onBindViewHolder(holder: AdminUserOrderDetailsViewHolder, position: Int) {
+
+        idPositionMap[productList[position].id] = position
+
         holder.productTitleTv.text = productList[position].title
         Picasso.get().load(productList[position].photoUrl).into(holder.productImageIv)
         holder.productImageIv.layoutParams.width = getScreenWidth() / 3
@@ -78,13 +82,17 @@ class AdminUserOrderDetailsAdapter : RecyclerView.Adapter<AdminUserOrderDetailsA
         holder.orderStatusTv.setTextColor(
             OrderStatusObject.getOrderColorFromString(productList[position].orderStatus))
 
-        orderOrderStatusButtonClickListener(holder)
-        orderDeliveryStatusButtonClickListener(holder)
+        orderOrderStatusButtonClickListener(holder,position)
+        orderDeliveryStatusButtonClickListener(holder,position)
 
     }
 
-    private fun orderOrderStatusButtonClickListener(holder: AdminUserOrderDetailsViewHolder) {
+    private fun orderOrderStatusButtonClickListener(
+        holder: AdminUserOrderDetailsViewHolder,
+        position: Int
+    ) {
         holder.orderStatusTv.setOnClickListener {
+            var calculatedPosition = idPositionMap[productList[position].id]
             if(holder.orderStatusTv.text.toString() == "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.paymentDone)}") {
                 Log.d(
                     "AdminUserOrderDetailsActivity",
@@ -92,21 +100,29 @@ class AdminUserOrderDetailsAdapter : RecyclerView.Adapter<AdminUserOrderDetailsA
                 )
                 holder.orderStatusTv.text = "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.refunded)}"
                 holder.orderStatusTv.setTextColor(OrderStatusObject.getOrderColor(OrderStatusObject.orderStatus.refunded))
+                AdminOrderSingletonClass.orderStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.orderStatusList[calculatedPosition].first,OrderStatusObject.orderStatus.refunded)
             } else if (holder.orderStatusTv.text.toString() == "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.refunded)}") {
                 holder.orderStatusTv.text = "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.paymentPending)}"
                 holder.orderStatusTv.setTextColor(OrderStatusObject.getOrderColor(OrderStatusObject.orderStatus.paymentPending))
+                AdminOrderSingletonClass.orderStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.orderStatusList[calculatedPosition].first,OrderStatusObject.orderStatus.paymentPending)
             } else if(holder.orderStatusTv.text.toString() == "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.paymentPending)}") {
                 holder.orderStatusTv.text = "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.paymentDone)}"
                 holder.orderStatusTv.setTextColor(OrderStatusObject.getOrderColor(OrderStatusObject.orderStatus.paymentDone))
+                AdminOrderSingletonClass.orderStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.orderStatusList[calculatedPosition].first,OrderStatusObject.orderStatus.paymentDone)
             } else if(holder.orderStatusTv.text.toString() == "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.error)}") {
                 holder.orderStatusTv.text = "Order Status = ${OrderStatusObject.getOrderString(OrderStatusObject.orderStatus.paymentDone)}"
                 holder.orderStatusTv.setTextColor(OrderStatusObject.getOrderColor(OrderStatusObject.orderStatus.paymentDone))
+                AdminOrderSingletonClass.orderStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.orderStatusList[calculatedPosition].first,OrderStatusObject.orderStatus.paymentDone)
             }
         }
     }
 
-    private fun orderDeliveryStatusButtonClickListener(holder: AdminUserOrderDetailsViewHolder) {
+    private fun orderDeliveryStatusButtonClickListener(
+        holder: AdminUserOrderDetailsViewHolder,
+        position: Int
+    ) {
         holder.deliveryStatusTv.setOnClickListener {
+            var calculatedPosition = idPositionMap[productList[position].id]
             if(holder.deliveryStatusTv.text.toString() == "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.paymentDone)}") {
                 Log.d(
                     "AdminUserOrderDetailsActivity",
@@ -114,18 +130,23 @@ class AdminUserOrderDetailsAdapter : RecyclerView.Adapter<AdminUserOrderDetailsA
                 )
                 holder.deliveryStatusTv.text = "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.delivered)}"
                 holder.deliveryStatusTv.setTextColor(OrderStatusObject.getDeliveryColor(OrderStatusObject.deliveryStatus.delivered))
+                AdminOrderSingletonClass.deliveryStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.deliveryStatusList[calculatedPosition].first,OrderStatusObject.deliveryStatus.delivered)
             } else if (holder.deliveryStatusTv.text.toString() == "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.delivered)}") {
                 holder.deliveryStatusTv.text = "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.cancelled)}"
                 holder.deliveryStatusTv.setTextColor(OrderStatusObject.getDeliveryColor(OrderStatusObject.deliveryStatus.cancelled))
+                AdminOrderSingletonClass.deliveryStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.deliveryStatusList[calculatedPosition].first,OrderStatusObject.deliveryStatus.cancelled)
             } else if(holder.deliveryStatusTv.text.toString() == "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.cancelled)}") {
                 holder.deliveryStatusTv.text = "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.returned)}"
                 holder.deliveryStatusTv.setTextColor(OrderStatusObject.getDeliveryColor(OrderStatusObject.deliveryStatus.returned))
+                AdminOrderSingletonClass.deliveryStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.deliveryStatusList[calculatedPosition].first,OrderStatusObject.deliveryStatus.returned)
             } else if(holder.deliveryStatusTv.text.toString() == "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.returned)}") {
                 holder.deliveryStatusTv.text = "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.paymentDone)}"
                 holder.deliveryStatusTv.setTextColor(OrderStatusObject.getDeliveryColor(OrderStatusObject.deliveryStatus.paymentDone))
+                AdminOrderSingletonClass.deliveryStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.deliveryStatusList[calculatedPosition].first,OrderStatusObject.deliveryStatus.paymentDone)
             } else if(holder.deliveryStatusTv.text.toString() == "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.error)}") {
                 holder.deliveryStatusTv.text = "Delivery Status = ${OrderStatusObject.getDeliveryString(OrderStatusObject.deliveryStatus.paymentDone)}"
                 holder.deliveryStatusTv.setTextColor(OrderStatusObject.getDeliveryColor(OrderStatusObject.deliveryStatus.paymentDone))
+                AdminOrderSingletonClass.deliveryStatusList[calculatedPosition!!] = Pair(AdminOrderSingletonClass.deliveryStatusList[calculatedPosition].first,OrderStatusObject.deliveryStatus.paymentDone)
             }
         }
     }
